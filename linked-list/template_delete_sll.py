545191c7dce0f0d7af69096402bb876dd2cbf9d3
Dhamodharan Sundararjan <30789057+Dhamodharan17@users.noreply.github.com>
2025-04-05 22:35:50 +0530

    Create template_delete_sll.py

---
+++ linked-list/template_delete_sll.py
@@ -0,0 +1,65 @@
+
+
+class LLNode:
+    def __init__(self, data = None):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
+
+    
+    def insert_at_end(self,data):
+        
+        #print
+    
+    def print(self):
+        
+        temp = self.head
+        while temp:
+            print(temp.data)
+            temp = temp.next
+    
+    def delete_at_start(self):
+        #print
+    
+        
+    def delete_at_end(self):
+        #print
+        
+       
+        
+    
+    def delete_at_pos(self, pos):
+        
+       #print
+        
+        
+        
+    def delete_at_before(self,before):
+       #print
+        
+        
+        
+    def delete_at_after(self,after):
+        #print
+    
+    
+        
+   
+
+#call
+ll = LinkedList()
+
+ll.delete_at_after(31)
+ll.print()
+    
+        
+    
+    
+    
+    
+        
+        
