7ef7d68613ac09fbd016605429c566204565becd
Dhamodharan Sundararjan <30789057+Dhamodharan17@users.noreply.github.com>
2025-04-05 22:34:01 +0530

    Create template_delete_dll.py

---
+++ linked-list/template_delete_dll.py
@@ -0,0 +1,47 @@
+
+class DLLNode:
+    def __init__(self, data = None):
+        self.data = data
+        self.prev = None
+        self.next = None
+        
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        
+    def insert_at_end(self,x):
+        #logic
+        
+       
+    
+    def print(self):
+        
+        temp = self.head
+        while temp:
+            print(temp.data)
+            temp = temp.next
+            
+            
+    def delete_at_start(self):
+        #logic
+    
+        
+    def delete_at_end(self):
+        #logic
+       
+    def delete_at_pos(self,pos):
+        #logic
+        
+        
+        
+    
+    def delete_before_node(x):
+        print('logic')
+        
+    def delete_after_node(x):
+        print('logic')
+        
+        
+dll = DoublyLinkedList()
+dll.insert_at_end(1)
+dll.print()
